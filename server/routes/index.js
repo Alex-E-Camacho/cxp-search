308fc141a7ba3b1b508d798efc3fc3b8b83dc8ca
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-08 12:42:41 -0700

    Successfully return bing web search api data with hardcoded search query

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,20 @@
 const searchController = require('../controllers').search;
+const axios = require('axios');
+const bingWebSearch = require('../helpers/bingWebSearh')
 
 module.exports = (app) => {
     app.get('/search', (req, res) => 
       res.send('React front end goes here'));
     
-    app.get('/search/results', (req, res) => 
-      res.send('results page route')
-  )
-};
+    app.get('/search/results', (req, res) =>
+      axios.all([
+        bingWebSearch.bingWebSearch('how to restart an azure vm through powerscript')
+      ])
+      .then(axios.spread((bing) => {
+        res.send(bing)
+      }))
+      .catch((error) => {
+        console.log(error);
+      }) 
+    )
+  };
