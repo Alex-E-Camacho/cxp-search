6fded29b1ee19b04c678d0ffc209faf7a3524a57
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-07 17:36:55 -0700

    Create /search/results route and send simple message

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,9 @@ const searchController = require('../controllers').search;
 
 module.exports = (app) => {
     app.get('/search', (req, res) => 
-      res.send('React front end goes here'))
+      res.send('React front end goes here'));
+    
+    app.get('/search/results', (req, res) => 
+      res.send('results page route')
+  )
 };
